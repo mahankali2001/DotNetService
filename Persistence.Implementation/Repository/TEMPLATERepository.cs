a9a7c8eb9a384ddf7e8113b7ab230b9ad070b5c4
Ram <ramprasad.mahankali@gmail.com>
2016-09-06 00:23:15 -0500

    From Ram

--- Persistence.Implementation/Repository/TEMPLATERepository.cs
+++ Persistence.Implementation/Repository/TEMPLATERepository.cs
@@ -47,6 +47,85 @@ namespace Persistence.Implementation.Repository
                                                                   this.DatabaseContext.CreateParameter("@Active", active)
                 );
         }
+
+        //public AppUser GetAppUser(int appUserId, params string[] includes)
+        //    {
+        //        var entitySet = from au in this.Read<AppUser>().Includes(includes)
+        //                        where au.AppUserId == appUserId
+        //                        select au;
+        //        return entitySet.FirstOrDefault();
+        //    }
+
+        //public DTOPage<VendorOnBoardingResponse> GetVendorOnBoarding(int pageIndex, int pageSize, VendorOnBoardingStatus status = VendorOnBoardingStatus.None)
+        //    {
+        //        return VendorOnBoardingBusiness.Initalize(this)
+        //            .GetNewVendorOnBoardingRecords(pageIndex, pageSize, status)
+        //            .ToMessage();
+        //    }
+
+        //public VendorOnBoardingBusiness GetNewVendorOnBoardingRecords(int pageindex = 1, int pagesize = 10, VendorOnBoardingStatus status = VendorOnBoardingStatus.None)
+        //{
+        //    //List<int> listOfUserRetailers = _infrastructureRepository.GetUserRetailerAssocated(DMContext.PartyId) ?? new List<int>();
+        //    List<int> listOfUserRetailers = GetRetailersAssociated();
+        //    PagedResult<VendorOnBoarding> vob = _infrastructureRepository.GetNewVendorOnBoardingRecords(status, listOfUserRetailers, pageindex, pagesize);
+        //    this.VendorOnBoardingCollections = ConvertToPageResultDTO(vob);
+        //    return this;
+        //}
+
+        //public PagedResult<VendorOnBoarding> GetNewVendorOnBoardingRecords(VendorOnBoardingStatus status, List<int> retailPartyId, int pageindex = 0, int pagesize = 10)
+        //{
+        //    if (status == VendorOnBoardingStatus.None)
+        //    {
+        //        if (retailPartyId.Count != 0)
+        //        {
+        //            var r1 =
+        //                from row in
+        //                    this.Read<VendorOnBoarding>().Where(
+        //                        x =>
+        //                        (retailPartyId.Contains(x.RetailPartyId) &&
+        //                         ((x.VendorOnBoardingStatusId != (byte)VendorOnBoardingStatus.CancelInvitation) &&
+        //                          (x.VendorOnBoardingStatusId != (byte)VendorOnBoardingStatus.Approve))
+        //                        ))
+        //                orderby row.LastSentDateTime descending
+        //                select row;
+        //            PagedResult<VendorOnBoarding> r1Result = Pager<VendorOnBoarding>.GetResult(r1, pageindex, pagesize);
+        //            return r1Result;
+        //        }
+        //        else
+        //        {
+        //            IQueryable<VendorOnBoarding> r3 = from row in
+        //                                                  this.Read<VendorOnBoarding>().Where(
+        //                                                      x => (((x.VendorOnBoardingStatusId != (byte)VendorOnBoardingStatus.CancelInvitation) &&
+        //                                                        (x.VendorOnBoardingStatusId !=
+        //                                                         (byte)VendorOnBoardingStatus.Approve))
+        //                                                      ))
+        //                                              orderby row.LastSentDateTime descending
+        //                                              select row;
+        //            PagedResult<VendorOnBoarding> r1Result = Pager<VendorOnBoarding>.GetResult(r3, pageindex, pagesize);
+        //            return r1Result;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        var s = new List<int> { (int)status };
+        //        var r2 = from row in
+        //                     this.Read<VendorOnBoarding>().Where(x => s.Contains(x.VendorOnBoardingStatusId) && retailPartyId.Contains(x.RetailPartyId))
+        //                 orderby row.LastSentDateTime descending
+        //                 select row;
+        //        PagedResult<VendorOnBoarding> result = Pager<VendorOnBoarding>.GetResult(r2, pageindex, pagesize);
+        //        return result;
+        //    }
+        //}
+
+        //internal static PagedResult<T> GetResult(IQueryable<T> query, int pageIndex, int pageSize)
+        //{
+        //    var result = new PagedResult<T> { CurrentPage = pageIndex, PageSize = pageSize, RowCount = query.Count() };
+        //    double pageCount = (double)result.RowCount / pageSize;
+        //    result.PageCount = (int)Math.Ceiling(pageCount);
+        //    int skip = (pageIndex - 1) * pageSize;
+        //    result.Results = query.Skip(skip).Take(pageSize).ToList();
+        //    return result;
+        //}
     }
 }
 
